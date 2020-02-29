e3d3fb146a088e072583b128dd2b46b661f0c26e
maverick1288 <harshalmayee1288@gmail.com>
2020-03-01 02:21:02 +0530

    Spring Boot Demo

---
+++ src/main/java/com/javaguides/springbootRESTHibernatePostgresCRUD/SpringbootRestHibernatePostgresCrudApplication.java
@@ -0,0 +1,13 @@
+package com.javaguides.springbootRESTHibernatePostgresCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestHibernatePostgresCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestHibernatePostgresCrudApplication.class, args);
+	}
+
+}
